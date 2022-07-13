aa5258450ef77ede84c02408d5053fbc7be021aa
Robin Guldener <r.guldener@gmail.com>
2022-07-13 14:38:02 +0200

    - First pass on Nango types - Add js-yaml dependency - Exclude nango-integrations from TS compiling - Re-add raw-msg-pusher and a sample LOAD_CONFIG msg

---
+++ nango-types.ts
@@ -0,0 +1,63 @@
+//////////////////////
+// Nango Config (nango-config.yaml)
+//////////////////////
+
+export interface NangoConfig {
+    nango_server_host: string,
+    nango_server_port: number,
+
+    nango_integrations_dir: string
+}
+
+//////////////////////
+// Integrations Config (integrations.yaml)
+//////////////////////
+
+export interface NangoIntegrationsConfig {
+    integrations: Array<NangoIntegrationConfig>
+}
+
+export enum NangoIntegrationAuthModes {
+    BASIC_AUTH,
+    OAUTH
+}
+
+export interface NangoIntegrationConfig {
+    base_url: string,
+    auth_mode: NangoIntegrationAuthModes,
+    call_auth: NangoCallAuth
+}
+
+export enum NangoCallAuthModes {
+    AUTH_HEADER_TOKEN
+}
+
+export interface NangoCallAuth {
+    mode: NangoCallAuthModes,
+    header_name?: string
+}
+
+//////////////////////
+// Client <-> Server Messages
+//////////////////////
+
+export enum NangoMessageAction {
+    LOAD_CONFIG = "LOAD_CONFIG",
+    REGISTER_CONNECTION = "REGISTER_CONNECTION",
+    TRIGGER_ACTION = "TRIGGER_ACTION"
+}
+
+export interface NangoMessage {
+    action: NangoMessageAction
+}
+
+export interface NangoLoadConfigMessage extends NangoMessage {
+    config: NangoConfig;
+}
+
+export interface NangoTriggerActionMessage extends NangoMessage {
+    integration: string,
+    triggeredAction: string,
+    userId: string,
+    input: object
+}
