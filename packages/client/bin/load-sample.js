e785776249f9b0e338a261d6009bddb55e01cff4
Robin Guldener <r.guldener@gmail.com>
2022-07-21 10:36:22 +0200

    Small fixes for containerization

--- packages/client/bin/load-sample.js
+++ packages/client/bin/load-sample.js
@@ -3,110 +3,91 @@ import Nango from '../dist/nango.js';
 /** -------------------- Utils -------------------- */
 
 function logResponse(integration, action, userId, response) {
-  console.log(
-    `${integration}.${action} response (userId: ${userId}): ${response.status} - ${response.statusText}`
-  );
+    console.log(`${integration}.${action} response (userId: ${userId}): ${response.status} - ${response.statusText}`);
 }
 
 function closeConnection() {
-  setTimeout(function () {
-    nango.close();
-    process.exit(0);
-  }, 1000);
+    setTimeout(function () {
+        nango.close();
+        process.exit(0);
+    }, 1000);
 }
 
 /** -------------------- Triger Actions -------------------- */
 
 async function slack() {
-  var integration = 'slack';
-  var action = 'notify';
-  var userId = '1';
-  var authToken = 'xoxb-2710526930471-3758349823251-Y8sw1nYPOpzI5yNOCtu6GbCc';
-  var channelId = 'C02MPPQC8FK';
-  var msg =
-    'Hello @channel, this is your friendly chat bot post to you through Nango :wave:';
+    var integration = 'slack';
+    var action = 'notify';
+    var userId = '1';
+    var authToken = 'xoxb-2710526930471-3758349823251-Y8sw1nYPOpzI5yNOCtu6GbCc';
+    var channelId = 'C02MPPQC8FK';
+    var msg = 'Hello @robin, this is your friendly chat bot post to you through Nango :wave:';
 
-  let registerConnectionPromise = nango.registerConnection(
-    integration,
-    userId,
-    authToken
-  );
-  registerConnectionPromise.catch((errorMsg) => {
-    console.log(`Uh oh, got error message on registerConnection: ${errorMsg}`);
-  });
+    let registerConnectionPromise = nango.registerConnection(integration, userId, authToken);
+    registerConnectionPromise.catch((errorMsg) => {
+        console.log(`Uh oh, got error message on registerConnection: ${errorMsg}`);
+    });
 
-  let result = await nango.trigger(integration, action, userId, {
-    channelId: channelId,
-    msg: msg
-  });
+    let result = await nango.trigger(integration, action, userId, {
+        channelId: channelId,
+        msg: msg
+    });
 
-  logResponse(integration, action, userId, result);
+    logResponse(integration, action, userId, result);
 }
 
 // Internal blueprint docs: https://www.notion.so/nangohq/Github-Blueprint-ec92750f43804677a44e92d1cda1db5f
 async function github() {
-  var integration = 'github';
-  var action = 'star';
-  var userId = '1';
-  var authToken = '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';
-  var owner = 'nodejs';
-  var repo = 'node';
+    var integration = 'github';
+    var action = 'star';
+    var userId = '1';
+    var authToken = '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';
+    var owner = 'nodejs';
+    var repo = 'node';
 
-  let registerConnectionPromise = nango.registerConnection(
-    integration,
-    userId,
-    authToken
-  );
-  registerConnectionPromise.catch((errorMsg) => {
-    console.log(`Uh oh, got error message on registerConnection: ${errorMsg}`);
-  });
+    let registerConnectionPromise = nango.registerConnection(integration, userId, authToken);
+    registerConnectionPromise.catch((errorMsg) => {
+        console.log(`Uh oh, got error message on registerConnection: ${errorMsg}`);
+    });
 
-  let result = await nango.trigger(integration, action, userId, {
-    owner: owner,
-    repo: repo
-  });
+    let result = await nango.trigger(integration, action, userId, {
+        owner: owner,
+        repo: repo
+    });
 
-  logResponse(integration, action, userId, result);
+    logResponse(integration, action, userId, result);
 }
 
 async function asana() {
-  var integration = 'asana';
-  var action = 'users';
-  var userId = '1';
-  var authToken = '1/1191314943604817:be1e3305fda920917fbd5a85240f578c';
+    var integration = 'asana';
+    var action = 'users';
+    var userId = '1';
+    var authToken = '1/1191314943604817:be1e3305fda920917fbd5a85240f578c';
 
-  let registerConnectionPromise = nango.registerConnection(
-    integration,
-    userId,
-    authToken
-  );
-  registerConnectionPromise.catch((errorMsg) => {
-    console.log(`Uh oh, got error message on registerConnection: ${errorMsg}`);
-  });
+    let registerConnectionPromise = nango.registerConnection(integration, userId, authToken);
+    registerConnectionPromise.catch((errorMsg) => {
+        console.log(`Uh oh, got error message on registerConnection: ${errorMsg}`);
+    });
 
-  let result = await nango.trigger(integration, action, userId, {});
+    let result = await nango.trigger(integration, action, userId, {});
 
-  logResponse(integration, action, userId, result);
+    logResponse(integration, action, userId, result);
 }
 
 async function hubspot() {
-  var integration = 'hubspot';
-  var action = 'contacts';
-  var userId = '1';
-  var authToken = 'pat-na1-c8dfe9b2-1bdc-449a-a2c6-fda036a2c030';
+    var integration = 'hubspot';
+    var action = 'contacts';
+    var userId = '1';
+    var authToken = 'pat-na1-c8dfe9b2-1bdc-449a-a2c6-fda036a2c030';
 
-  let registerConnectionPromise = nango.registerConnection(
-    integration,
-    userId,
-    authToken
-  );
-  registerConnectionPromise.catch((errorMsg) => {
-    console.log(`Uh oh, got error message on registerConnection: ${errorMsg}`);
-  });
+    let registerConnectionPromise = nango.registerConnection(integration, userId, authToken);
+    registerConnectionPromise.catch((errorMsg) => {
+        console.log(`Uh oh, got error message on registerConnection: ${errorMsg}`);
+    });
 
-  let result = await nango.trigger(integration, action, userId, { limit: 5 });
+    let result = await nango.trigger(integration, action, userId, { limit: 5 });
 
-  logResponse(integration, action, userId, result);
+    logResponse(integration, action, userId, result);
 }
 
 /** -------------------- Execution -------------------- */
@@ -117,11 +98,9 @@ await nango.connect();
 const fn = process.argv[2];
 
 if (typeof fn !== 'string') {
-  console.log(
-    'Provided parameter does not correspond to sample test function. Please call: node run sample <sample-test-function-name>'
-  );
+    console.log('Provided parameter does not correspond to sample test function. Please call: node run sample <sample-test-function-name>');
 } else {
-  eval(fn)();
+    eval(fn)();
 }
 
 closeConnection();
