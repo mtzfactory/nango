276ff568e613963fef1e820ab3ecfd620a5a2117
Bastien Beurier <bastienbeurier@gmail.com>
2022-07-22 10:33:17 +0200

    CLI watch command + init folder reflects tutorial

--- packages/cli/lib/index.ts
+++ packages/cli/lib/index.ts
@@ -4,6 +4,7 @@ import { Command } from 'commander';
 import * as fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import * as child_process from 'child_process';
 
 const program = new Command();
 
@@ -16,6 +17,13 @@ program
         initCommand();
     });
 
+program
+    .command('watch')
+    .description('Compile typescript files on save.')
+    .action(() => {
+        watchCommand();
+    });
+
 program.parse();
 
 function initCommand() {
@@ -25,4 +33,9 @@ function initCommand() {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
     fs.cpSync(`${__dirname}/../nango-integrations`, './nango-integrations', { recursive: true });
+}
+
+function watchCommand() {
+    console.log('\n👀 Compiling on save. Please keep this terminal window open.\n');
+    child_process.execSync('./node_modules/typescript/bin/tsc ./**/*.ts --outDir ./dist --rootDir . -t es2022 --moduleResolution node --watch');
 }
