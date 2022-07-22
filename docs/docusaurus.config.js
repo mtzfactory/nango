743f0c8e91a03bb814555a515f0b8637a763bae6
Bastien Beurier <bastienbeurier@gmail.com>
2022-07-22 18:42:27 +0200

    Fix URL

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Nango Docs',
   tagline: 'Documentation for the Nango framework',
-  url: 'https://nango.github.io',
+  url: 'https://nangohq.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
