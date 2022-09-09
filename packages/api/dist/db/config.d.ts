5bf76e9451d8c3e5fa5bcedb158d5f160fcf89ed
Bastien Beurier <bastienbeurier@gmail.com>
2022-09-09 12:26:00 +0200

    Compile remotely and untrack dist folder

--- packages/api/dist/db/config.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Knex } from 'knex';
-declare let config: {
-    development: Knex.Config<any>;
-    production: Knex.Config<any>;
-    onUpdateTrigger: any;
-};
-export { config };
--- packages/api/dist/lib/api/contacts/contacts.routes.config.d.ts
+++
@@ -1,6 +0,0 @@
-import { CommonRoutesConfig } from '../common/common.routes.config.js';
-import type express from 'express';
-export declare class ContactsRoutes extends CommonRoutesConfig {
-    constructor(app: express.Application);
-    configureRoutes(): express.Application;
-}
