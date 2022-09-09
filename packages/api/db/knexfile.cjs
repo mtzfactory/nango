6aa2216776b0d999e5fdd98bc22a79e6f27a7705
Bastien Beurier <bastienbeurier@gmail.com>
2022-09-09 09:02:55 +0200

    Different ssl parameter format

--- packages/api/db/knexfile.cjs
+++ packages/api/db/knexfile.cjs
@@ -17,7 +17,7 @@ module.exports = {
 
     production: {
         client: 'pg',
-        connection: (process.env['DATABASE_URL'] || '') + '?ssl=true',
+        connection: (process.env['DATABASE_URL'] || '') + '?sslmode=require',
         migrations: {
             directory: './migrations',
             extension: 'ts'
