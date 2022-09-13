02657e39a159945cc94540edf6004cf307665204
Bastien Beurier <bastienbeurier@gmail.com>
2022-09-13 17:05:46 +0200

    Add params to get contacts endpoint

--- packages/api/lib/api/crm/v1/contacts/contacts.controller.ts
+++ packages/api/lib/api/crm/v1/contacts/contacts.controller.ts
@@ -1,9 +1,33 @@
 import type express from 'express';
 import usersService from '../../../../shared/services/contacts.service.js';
 
+export interface ListContactsParams {
+    account_id: string | undefined;
+    external_id: string | undefined;
+    include_raw: boolean | undefined;
+    page_size: number | undefined;
+    cursor: string | undefined;
+    modified_after: Date | undefined;
+    modified_before: Date | undefined;
+    created_after: Date | undefined;
+    created_before: Date | undefined;
+}
+
 class ContactsController {
-    async listContacts(_: express.Request, res: express.Response) {
-        const contacts = await usersService.list();
+    async listContacts(req: express.Request, res: express.Response) {
+        let params: ListContactsParams = {
+            account_id: typeof req.query['account_id'] === 'string' ? req.query['account_id'] : undefined,
+            external_id: typeof req.query['external_id'] === 'string' ? req.query['external_id'] : undefined,
+            include_raw: req.query['include_raw'] == null ? undefined : req.query['include_raw'] === 'true',
+            page_size: req.query['page_size'] == null ? undefined : isNaN(+req.query['page_size']) ? undefined : +req.query['page_size'],
+            cursor: typeof req.query['account_id'] === 'string' ? req.query['account_id'] : undefined,
+            modified_after: typeof req.query['modified_after'] === 'string' ? new Date(req.query['modified_after']) : undefined,
+            modified_before: typeof req.query['modified_before'] === 'string' ? new Date(req.query['modified_before']) : undefined,
+            created_after: typeof req.query['created_after'] === 'string' ? new Date(req.query['created_after']) : undefined,
+            created_before: typeof req.query['created_before'] === 'string' ? new Date(req.query['created_before']) : undefined
+        };
+
+        const contacts = await usersService.list(params);
         res.status(200).send(contacts);
     }
 }
--- packages/api/lib/shared/services/contacts.service.ts
+++ packages/api/lib/shared/services/contacts.service.ts
@@ -1,9 +1,37 @@
 import db from '../database.js';
 import type { Contact } from '../models/contact.model.js';
+import type { ListContactsParams } from '../../api/crm/v1/contacts/contacts.controller.js';
 
 class ContactsService {
-    async list() {
-        return await db.knex<Contact>('contacts').limit(100);
+    async list(params: ListContactsParams) {
+        return await db
+            .knex<Contact>('contacts')
+            .modify(function (queryBuilder) {
+                if (params.account_id != null) {
+                    queryBuilder.where('account', params.account_id);
+                }
+
+                if (params.external_id != null) {
+                    queryBuilder.where('external_id', params.external_id);
+                }
+
+                if (params.modified_after != null) {
+                    queryBuilder.where('updated_at', '>=', params.modified_after);
+                }
+
+                if (params.modified_before != null) {
+                    queryBuilder.where('updated_at', '<', params.modified_before);
+                }
+
+                if (params.created_after != null) {
+                    queryBuilder.where('created_at', '>=', params.created_after);
+                }
+
+                if (params.created_before != null) {
+                    queryBuilder.where('created_at', '<', params.created_before);
+                }
+            })
+            .limit(params.page_size == null ? 10 : Math.min(params.page_size, 100));
     }
 
     public async upsertFromList(contacts: Contact[]): Promise<void | number[]> {
