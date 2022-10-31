39f73bd1ac9e865469e2f4dc410f45f3c5b39b90
Bastien Beurier <bastienbeurier@gmail.com>
2022-10-31 14:27:41 +0100

    Update Node SDK

--- packages/core/lib/models/sync.model.ts
+++ packages/core/lib/models/sync.model.ts
@@ -2,14 +2,14 @@ import type { AxiosRequestHeaders } from 'axios';
 
 export interface Sync {
     id?: number;
+    created_at?: Date;
+    updated_at?: Date;
     url: string;
+    method: string;
     headers?: AxiosRequestHeaders;
     body?: object;
     unique_key: string;
     response_path?: string;
-    created_at?: Date;
-    updated_at?: Date;
-    method: string;
     paging_request_path?: string;
     paging_response_path?: string;
     paging_url_path?: string;
--- packages/node-client/lib/nango.ts
+++ packages/node-client/lib/nango.ts
@@ -9,6 +9,8 @@ export enum NangoHttpMethod {
 }
 
 export interface NangoSyncConfig {
+    nango_server?: string;
+    nango_port?: string;
     method?: NangoHttpMethod;
     headers?: object;
     body?: object;
@@ -16,8 +18,8 @@ export interface NangoSyncConfig {
     response_path?: string;
     paging_request_path?: string;
     paging_response_path?: string;
-    nango_server?: string;
-    nango_port?: string;
+    paging_url_path?: string;
+    max_total?: number;
 }
 
 export class Nango {
@@ -41,7 +43,9 @@ export class Nango {
             unique_key: config.unique_key,
             response_path: config.response_path,
             paging_request_path: config.paging_request_path,
-            paging_response_path: config.paging_response_path
+            paging_response_path: config.paging_response_path,
+            paging_url_path: config.paging_url_path,
+            max_total: config.max_total
         };
 
         return await axios.post(nango_req_url, nango_req_body, { headers: nango_req_headers });
