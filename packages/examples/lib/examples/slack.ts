9679341c2107a6897a12dd963b863215bad7fe58
Bastien Beurier <bastienbeurier@gmail.com>
2022-11-03 11:37:28 +0100

    Nits

--- packages/examples/lib/examples/slack.ts
+++ packages/examples/lib/examples/slack.ts
@@ -1,14 +1,14 @@
 import { Nango } from '@nangohq/node-client';
 
-// CLI command: npm start syncPokemonList
+// CLI command: npm start syncSlackMessages [app-token] [channel-id]
 // Endpoint docs: https://api.slack.com/methods/conversations.history
-export let syncSlackMessages = async (api_token: string, channel_id: string) => {
+export let syncSlackMessages = async (app_token: string, channel_id: string) => {
     let config = {
         response_path: 'messages',
         paging_response_path: 'response_metadata.next_cursor',
         paging_request_path: 'cursor',
         headers: {
-            authorization: `Bearer ${api_token}`
+            authorization: `Bearer ${app_token}`
         },
         query_params: {
             channel: channel_id,
